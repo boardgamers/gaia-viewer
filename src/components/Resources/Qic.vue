44edcec0a5089eb035745ee856224b720e56111d
coyotte508 <coyotte508@gmail.com>
2020-06-24 00:58:55 +0200

    Improve qic

--- src/components/Resources/Qic.vue
+++ src/components/Resources/Qic.vue
@@ -1,5 +1,5 @@
 <template>
   <g>
-    <image xlink:href="../../assets/resources/qic.svg" width=20 x=-10 y=-10 />
+    <image xlink:href="../../assets/resources/qic.svg" width=20 x=-10 y=-9 />
   </g>
 </template>
