a9a921caaa2a3dccc5d280aae0d4f0d5524a0876
coyotte508 <coyotte508@gmail.com>
2020-06-24 12:09:27 +0200

    Add round scorings

---
+++ src/components/Operator.vue
@@ -0,0 +1,26 @@
+<template>
+  <g class="operator">
+    <image xlink:href="../assets/operators/trigger.svg" width=15 transform=translate(-20,-24) />
+  </g>
+</template>
+<script lang="ts">
+import { Vue, Component, Prop, Watch } from "vue-property-decorator";
+import { Condition as ConditionEnum, Building as BuildingEnum, Operator as OperatorEnum } from "@gaia-project/engine";
+
+@Component({
+  components: {
+  }
+})
+export default class Operator extends Vue {
+  @Prop()
+  condition!: ConditionEnum;
+
+  @Prop()
+  operator: OperatorEnum;
+
+  get isTrigger () {
+    return this.operator === OperatorEnum.Trigger;
+  }
+}
+
+</script>
